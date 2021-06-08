caf786475b1d42b8a3da0c85373898083b2a7857
Mickael Savinaud <mickael.savinaud@c-s.fr>
2021-06-08 16:27:55 +0200

    move to 0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="dataship",
-    version="0.0.1",
+    version="0.1",
     description="Data access using eodag",
     author="Fahd Benatia",
     author_email="fahd.benatia@csgroup.eu",
