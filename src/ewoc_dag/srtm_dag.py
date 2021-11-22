140f7a7e1bcbde9c839a2864904baecaad3d6d25
Mickael Savinaud <mickael.savinaud@c-s.fr>
2021-11-22 09:22:24 +0100

    update to bucket and black

--- src/ewoc_dag/srtm_dag.py
+++ src/ewoc_dag/srtm_dag.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+""" DAG for SRTM 1s and 3s tiles
+"""
 import logging
 from pathlib import Path
 from typing import List
@@ -6,15 +9,16 @@ import zipfile
 import requests
 from eotile.eotile_module import main
 
-from ewoc_dag.provider.creodias import CREODIASDataProvider
-from ewoc_dag.provider.ewoc import EWOCDataProvider
+from ewoc_dag.bucket.creodias import CreodiasBucket
+from ewoc_dag.bucket.ewoc import EWOCAuxDataBucket
 
 
 logger = logging.getLogger(__name__)
 
 
-def get_srtm_from_s2_tile_id(s2_tile_id: str, out_dirpath: Path,
-                             source: str = "esa", resolution='1s') -> None:
+def get_srtm_from_s2_tile_id(
+    s2_tile_id: str, out_dirpath: Path, source: str = "esa", resolution="1s"
+) -> None:
     """
     Retrieve srtm 1s data for a Sentinel-2 tile id from the source into the output dir
     :param s2_tile_ids: Sentinel-2 tile id
@@ -22,12 +26,14 @@ def get_srtm_from_s2_tile_id(s2_tile_id: str, out_dirpath: Path,
     :param source: Source where to retrieve the srtm 1s data
     :param resolution: 1s or 3s for respectively 30m and 90m srtm
     """
-    get_srtm_tiles(get_srtm1s_ids(s2_tile_id), out_dirpath,
-                   source=source, resolution=resolution)
+    get_srtm_tiles(
+        get_srtm1s_ids(s2_tile_id), out_dirpath, source=source, resolution=resolution
+    )
 
 
-def get_srtm_tiles(srtm_tile_ids: List[str], out_dir: Path,
-                    source: str = "esa", resolution='1s') -> None:
+def get_srtm_tiles(
+    srtm_tile_ids: List[str], out_dir: Path, source: str = "esa", resolution="1s"
+) -> None:
     """
     Retrieve srtm 1s data from the source into the output dir
     :param srtm_tile_ids: List of srtm tile ids
@@ -43,20 +49,25 @@ def get_srtm_tiles(srtm_tile_ids: List[str], out_dir: Path,
     elif source == "creodias":
         if resolution == "1s":
             logger.info("Use creodias bucket to retrieve srtm 1s data!")
-            creo_s3 = CREODIASDataProvider()
-            creo_s3.download_srtm1s_tiles(srtm_tile_ids, out_dir)
+            CreodiasBucket().download_srtm1s_tiles(srtm_tile_ids, out_dir)
         else:
-            raise ValueError(f"Source SRTM{resolution} not available on CREODIAS bucket!")
+            raise ValueError(
+                f"Source SRTM{resolution} not available on CREODIAS bucket!"
+            )
     elif source == "ewoc":
-        logger.info("Use EWoC bucket to retrieve srtm data!")
-        ewoc_s3 = EWOCDataProvider()
-        ewoc_s3.download_srtm_tiles(srtm_tile_ids, out_dir,
-                                               resolution=resolution)
+        if resolution == "3s":
+            logger.info("Use EWoC bucket to retrieve srtm data!")
+            EWOCAuxDataBucket.download_srtm3s_tiles(srtm_tile_ids, out_dir)
+        else:
+            raise ValueError(
+                f"Source SRTM{resolution} not available on EWoC Aux Data bucket!"
+            )
     elif source == "usgs":
         logger.info("Use usgs EE to retrieve srtm 1s data!")
         raise NotImplementedError
     else:
         raise ValueError(f"Source {source} not supported for srtm!")
+
 
 def get_srtm_from_esa(srtm_tile_ids: List[str], out_dirpath: Path) -> None:
     """
@@ -64,12 +75,13 @@ def get_srtm_from_esa(srtm_tile_ids: List[str], out_dirpath: Path) -> None:
     :param srtm_tile_ids: List of srtm tile ids
     :param out_dirpath: Output directory where the srtm data is downloaded
     """
-    ESA_WEBSITE_ROOT = "http://step.esa.int/auxdata/dem/SRTMGL1/"
+    esa_website_root = "http://step.esa.int/auxdata/dem/SRTMGL1/"
     for srtm_tile_id in srtm_tile_ids:
         srtm_tile_id_filename = srtm_tile_id + ".SRTMGL1.hgt.zip"
-        srtm_tile_id_url = ESA_WEBSITE_ROOT + srtm_tile_id_filename
+        srtm_tile_id_url = esa_website_root + srtm_tile_id_filename
 
         response = requests.get(srtm_tile_id_url)
+        # pylint: disable=no-member
         if response.status_code != requests.codes.ok:
             logger.error(
                 "%s not dwnloaded (error_code: %s) from %s!",
@@ -89,6 +101,7 @@ def get_srtm_from_esa(srtm_tile_ids: List[str], out_dirpath: Path) -> None:
             srtm_zipfile.extractall(out_dirpath)
 
         srtm_tile_id_filepath.unlink()
+
 
 def get_srtm1s_ids(s2_tile_id: str) -> None:
     """
