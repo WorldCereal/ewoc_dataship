33073de9c6cd44d387ae8e9459c82d7e4cd9cd32
Mickael Savinaud <mickael.savinaud@c-s.fr>
2022-01-20 15:19:13 +0100

    fix landsat8 dag

--- src/ewoc_dag/landsat8.py
+++ src/ewoc_dag/landsat8.py
@@ -11,7 +11,7 @@ from ewoc_dag.bucket.aws import AWSL8C2L2Bucket
 from ewoc_dag.eodag_utils import get_product_by_id
 
 
-logger = logging.getLogger(__name__)
+_logger = logging.getLogger(__name__)
 
 _L8C2_SOURCES = ["eodag", "aws"]
 
@@ -32,9 +32,11 @@ def get_l8c2l2_product(
     """
 
     if source == _L8C2_SOURCES[0]:
-        logging.info("Use EODAG to retrieve Landsat 8 L2 C2 product!")
-        if l2_mask_only:
-            logger.warning("EODAG does not support to retrieve l2 mask only!")
+        _logger.info("Use EODAG to retrieve Landsat 8 L2 C2 product!")
+        if prd_items is not None:
+            _logger.warning(
+                "EODAG does not support to retrieve element of the product!"
+            )
         out_prd_path = get_product_by_id(
             prd_id,
             out_root_dirpath,
@@ -43,7 +45,7 @@ def get_l8c2l2_product(
             product_type="TODO",
         )
     elif source == _L8C2_SOURCES[1]:
-        logging.info("Use AWS to retrieve Landsat 8 L2 C2 product!")
+        _logger.info("Use AWS to retrieve Landsat 8 L2 C2 product!")
         out_prd_path = AWSL8C2L2Bucket().download_prd(prd_id, out_root_dirpath)
     else:
         raise ValueError(f"Source {source} is not supported: not in {_L8C2_SOURCES}")
@@ -63,4 +65,4 @@ if __name__ == "__main__":
     )
 
     _PRD_ID = "LC08_L2SP_227099_20211017_20211026_02_T2"
-    get_l8_product(_PRD_ID)
+    get_l8c2l2_product(_PRD_ID)
